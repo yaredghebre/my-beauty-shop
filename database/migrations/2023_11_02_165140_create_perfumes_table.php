61b401621cabb7f3cc89a39e6ed60f47102b5d55
yaredghebre <yared2391@gmail.com>
2023-11-02 18:14:15 +0100

    add model and perfume table

---
+++ database/migrations/2023_11_02_165140_create_perfumes_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('perfumes', function (Blueprint $table) {
+            $table->id();
+            $table->string('title', 150)->unique();
+            $table->string('gender', 20);
+            $table->string('brand', 50);
+            $table->string('size', 10);
+            $table->decimal('price', 4, 2);
+            $table->text('description')->nullable();
+            $table->boolean('available')->default(true);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('perfumes');
+    }
+};
