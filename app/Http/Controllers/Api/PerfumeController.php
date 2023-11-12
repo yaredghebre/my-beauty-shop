9b5d1080028d098dc1a211af76a9fb5a3247f6ef
yaredghebre <yared2391@gmail.com>
2023-11-12 19:45:10 +0100

    Api tested on Postman OK

--- app/Http/Controllers/Api/PerfumeController.php
+++ app/Http/Controllers/Api/PerfumeController.php
@@ -3,9 +3,37 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Perfume;
 use Illuminate\Http\Request;
 
 class PerfumeController extends Controller
 {
-    //
+    public function index(Request $request)
+    {
+        if ($request->has('category_id')) {
+            $perfumes = Perfume::with('category')->where('category_id', $request->category_id)->paginate(10);
+        } else {
+            $perfumes = Perfume::with('category')->paginate(10);
+        }
+        return response()->json([
+            'success' => true,
+            'results' => $perfumes
+        ]);
+    }
+
+    public function show($id)
+    {
+        $perfume = Perfume::with('category')->where('id', $id)->first();
+        if ($perfume) {
+            return response()->json([
+                'success' => true,
+                'results' => $perfume
+            ]);
+        } else {
+            return response()->json([
+                'success' => false,
+                'results' => 'Nessun profumo trovato'
+            ])->setStatusCode(404);
+        }
+    }
 }
